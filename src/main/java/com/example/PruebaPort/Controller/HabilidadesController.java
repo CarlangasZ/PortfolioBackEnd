03a9ea890d2a2426fee0296cde7b57c90f4ccb8c
CarlangasZ <carlodherefia308@gmail.com>
2023-03-29 00:54:41 -0300

    Se realizaron cambios en los controladores para permitir el acceso de FireBase

--- src/main/java/com/example/PruebaPort/Controller/HabilidadesController.java
+++ src/main/java/com/example/PruebaPort/Controller/HabilidadesController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/habilidades")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://portfolio-carlosheredia.web.app")
 public class HabilidadesController {
  
      @Autowired
